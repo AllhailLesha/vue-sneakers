b6446179fdcb59bebab5c80497181ccd4821610f
AllhailLesha <a.goponenko0098@gmail.com>
2024-03-17 20:44:17 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <div class="mb-2 text-slate-700">
+    <MyHello title="new balance" :price="11000" :isAdded="true"> </MyHello>
+  </div>
+</template>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
+
+</script>
+
+<template>
+  <div class="product">
+    <h2>Товар: {{ title }}</h2>
+    <h3>Цена: {{ price }}</h3>
+    <h4>Добавлен: {{ isAdded }}</h4>
+
+    <slot></slot>
+  </div>
+</template>
+
+<style scope>
+.product {
+  border: 1px solid teal;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
